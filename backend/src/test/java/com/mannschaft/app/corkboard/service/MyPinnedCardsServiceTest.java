ca0c145964847828da370eab2cdf6f56e9b63916
kenta <hideharu215@yahoo.co.jp>
2026-05-06 12:11:25 +0900

    修正: Spy インポートを誤削除した問題を修正

--- backend/src/test/java/com/mannschaft/app/corkboard/service/MyPinnedCardsServiceTest.java
+++ backend/src/test/java/com/mannschaft/app/corkboard/service/MyPinnedCardsServiceTest.java
@@ -17,6 +17,7 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Spy;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.data.domain.Pageable;
 import org.springframework.test.util.ReflectionTestUtils;
