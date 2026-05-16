83ac73041f67dd28339ec960872c76d4aeee0403
kenta <hideharu215@yahoo.co.jp>
2026-05-16 13:34:32 +0900

    fix(U10): PostingIdentityServiceTest を MockitoSettings.LENIENT に変更 (拡張後 unnecessary stubbing 解消)

--- backend/src/test/java/com/mannschaft/app/village/service/PostingIdentityServiceTest.java
+++ backend/src/test/java/com/mannschaft/app/village/service/PostingIdentityServiceTest.java
@@ -28,6 +28,8 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
@@ -59,6 +61,7 @@ import static org.mockito.BDDMockito.given;
  * </ul>
  */
 @ExtendWith(MockitoExtension.class)
+@MockitoSettings(strictness = Strictness.LENIENT)
 @DisplayName("PostingIdentityService 単体テスト")
 class PostingIdentityServiceTest {
 
